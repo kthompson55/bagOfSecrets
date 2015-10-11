5b96d853c595455df4d337bf0a98cc1530b26046
Karl Thompson <karlthompsonnu@gmail.com>
2015-10-10 21:49:57 -0600

    Add in structure for dialogue, I guess

--- Assets/Scripts/Character/ConversationPartner.cs
+++ Assets/Scripts/Character/ConversationPartner.cs
@@ -5,4 +5,7 @@ public class ConversationPartner : MonoBehaviour
 {
     public Pickup.PickupType secretType;
     public string name;
+    public string[] conversation;
+    public bool[] playerSpeaking;
+    public string[] responses = new string[4];
 }
